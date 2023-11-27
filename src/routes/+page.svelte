d314fedf923a9acceb4c9aec3a4348e5fe681fab
fran <francobarrientos56@gmail.com>
2023-11-27 18:40:14 -0300

    netlify w4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -128,21 +128,21 @@
 			<form class="mt-4 flex flex-col"  method="POST" name="contact" data-netlify="true">
 				<div class="mb-3">
 					<Label for="name" class="block mb-2">Name</Label>
-					<Input required id="name" placeholder="Your Name" />
+					<Input  required id="name" name="name" placeholder="Your Name" />
 				</div>
 				<div class="mb-3">
 					<Label for="email" class="block mb-2">Your Email</Label>
-					<Input required id="email" type="email" placeholder="name@flowbite.com">
+					<Input required id="email" name="email"  type="email" placeholder="name@flowbite.com">
 						<Envelope slot="left" width="w-5" heigth="h-5" />
 					</Input>
 				</div>
 
 				<div class="mb-3">
-					<Label for="email" class="block mb-2">Your Message</Label>
-					<Input required id="email" type="text" placeholder="Hi Fran !!"></Input>
+					<Label for="msj" class="block mb-2">Your Message</Label>
+					<Input required id="msj" name="msj"  type="text" placeholder="Hi Fran !!"></Input>
 				</div>
 				<Button type="submit" size="sm"
-					><Envelope width="w-3" heigth="h-3" /><span class="ml-2">Send Email</span></Button
+					><Envelope width="w-3" heigth="h-3" /><span class="ml-2">Send messagge</span></Button
 				>
 			</form>
 		</div>
