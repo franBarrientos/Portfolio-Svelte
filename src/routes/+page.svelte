ccf4c73f312c1d6a908a924bdfc7e94135306bec
Fran Barrientos <117659602+franBarrientos@users.noreply.github.com>
2023-11-28 04:42:48 +0000

    final

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -122,7 +122,6 @@
 		</div>
 	</div>
 
-	<input type="hidden" name="contact" value="contact" />
 
 
 	<div
@@ -135,18 +134,19 @@
 				<input type="hidden" name="form-name" value="contact" />
 				<div class="mb-3">
 					<Label for="name" class="block mb-2">Name</Label>
-					<Input  required id="name" name="name" placeholder="Your Name" />
+					<input class='block w-full disabled:cursor-not-allowed disabled:opacity-50 rounded-lg p-2'
+  required id="name" name="name" placeholder="Your Name" />
 				</div>
 				<div class="mb-3">
 					<Label for="email" class="block mb-2">Your Email</Label>
-					<Input required id="email" name="email"  type="email" placeholder="name@flowbite.com">
-						<Envelope slot="left" width="w-5" heigth="h-5" />
-					</Input>
+					<input class='block w-full disabled:cursor-not-allowed disabled:opacity-50 rounded-lg p-2'
+ required id="email" name="email"  type="email" placeholder="name@flowbite.com"/>
 				</div>
 
 				<div class="mb-3">
 					<Label for="msj" class="block mb-2">Your Message</Label>
-					<Input required id="msj" name="msj"  type="text" placeholder="Hi Fran !!"></Input>
+					<input class='block w-full disabled:cursor-not-allowed disabled:opacity-50 rounded-lg p-2'
+ required id="msj" name="msj"  type="text" placeholder="Hi Fran !!"/>
 				</div>
 				<Button type="submit" size="sm"
 					><Envelope width="w-3" heigth="h-3" /><span class="ml-2">Send messagge</span></Button
